9c6449d97dd2fc60b3b21167e50146b11f24f3c1
John Colcombe <jcolcomb@cisco.com>
2021-06-22 16:38:14 +0100

    new

---
+++ main.tf
@@ -0,0 +1,269 @@
+
+variable "globalwsname" {
+  type = string
+}
+
+variable "ikswsname" {
+  type = string
+}
+output "ikswsname" {
+        value = var.ikswsname
+}
+#Output all global variables
+variable "password" {
+  type = string
+}
+output "password" {
+        value = var.password
+	sensitive = true
+}
+variable "clustername" {
+  type = string
+}
+output "clustername" {
+        value = var.clustername
+}
+variable "ippool_list" {
+  type = string
+}
+output "ippool_list" {
+        value = var.ippool_list
+}
+
+variable "infra_config_policy" {
+  type = string
+}
+output "infra_config_policy" {
+        value = var.infra_config_policy
+}
+variable "ip_pool_policy" {
+  type = string
+}
+output "ip_pool_policy" {
+        value = var.ip_pool_policy
+}
+variable "masterip_pool_policy" {
+  type = string
+}
+output "masterip_pool_policy" {
+        value = var.masterip_pool_policy
+}
+variable "workerip_pool_policy" {
+  type = string
+}
+output "workerip_pool_policy" {
+        value = var.workerip_pool_policy
+}
+variable "network_pod" {
+  type = string
+}
+output "network_pod" {
+        value = var.network_pod
+}
+variable "network_service" {
+  type = string
+}
+output "network_service" {
+        value = var.network_service
+}
+variable "k8s_version" {
+  type = string
+}
+output "k8s_version" {
+        value = var.k8s_version
+}
+variable "k8s_version_name" {
+  type = string
+}
+output "k8s_version_name" {
+        value = var.k8s_version_name
+}
+variable "masterinstance" {
+  type = string
+}
+output "masterinstance" {
+        value = var.masterinstance
+}
+variable "mgmtcfgetcd" {
+  type = string
+}
+output "mgmtcfgetcd" {
+        value = var.mgmtcfgetcd
+}
+variable "mgmtcfglbcnt" {
+  type = string
+}
+output "mgmtcfglbcnt" {
+        value = var.mgmtcfglbcnt
+}
+variable "mgmtcfgsshuser" {
+  type = string
+}
+output "mgmtcfgsshuser" {
+        value = var.mgmtcfgsshuser
+}
+variable "mastergrpname" {
+  type = string
+}
+output "mastergrpname" {
+        value = var.mastergrpname
+}
+variable "masterdesiredsize" {
+  type = string
+}
+output "masterdesiredsize" {
+        value = var.masterdesiredsize
+}
+variable "workergrpname" {
+  type = string
+}
+output "workergrpname" {
+        value = var.workergrpname
+}
+variable "workerdesiredsize" {
+  type = string
+}
+output "workerdesiredsize" {
+        value = var.workerdesiredsize
+}
+variable "masterinfraname" {
+  type = string
+}
+output "masterinfraname" {
+        value = var.masterinfraname
+}
+variable "infrapolname" {
+  type = string
+}
+output "infrapolname" {
+        value = var.infrapolname
+}
+variable "instancetypename" {
+  type = string
+}
+output "instancetypename" {
+        value = var.instancetypename
+}
+variable "device_name" {
+  type = string
+}
+output "device_name" {
+        value = var.device_name
+}
+variable "portgroup" {
+  type = string
+}
+output "portgroup" {
+        value = var.portgroup
+}
+variable "datastore" {
+  type = string
+}
+output "datastore" {
+        value = var.datastore
+}
+variable "vspherecluster" {
+  type = string
+}
+output "vspherecluster" {
+        value = var.vspherecluster
+}
+
+variable "resource_pool" {
+  type = string
+}
+output "resource_pool" {
+        value = var.resource_pool
+}
+
+variable "starting_address" {
+  type = string
+}
+output "starting_address" {
+        value = var.starting_address
+}
+
+
+variable "pool_size" {
+  type = string
+}
+output "pool_size" {
+        value = var.pool_size
+}
+
+
+
+variable "netmask" {
+  type = string
+}
+output "netmask" {
+        value = var.netmask
+}
+
+variable "gateway" {
+  type = string
+}
+output "gateway" {
+        value = var.gateway
+}
+
+
+variable "primary_dns" {
+  type = string
+}
+output "primary_dns" {
+        value = var.primary_dns
+}
+
+
+
+variable "timezone" {
+  type = string
+}
+output "timezone" {
+        value = var.timezone
+}
+
+
+
+
+variable "domain_name" {
+  type = string
+}
+output "domain_name" {
+        value = var.domain_name
+}
+
+
+
+variable "cpu" {
+  type = string
+}
+output "cpu" {
+        value = var.cpu
+}
+
+
+
+variable "disk_size" {
+  type = string
+}
+output "disk_size" {
+        value = var.disk_size
+}
+
+
+variable "memory" {
+  type = string
+}
+output "memory" {
+        value = var.memory
+}
+
+
+variable "organization" {
+  type = string
+}
+output "organization" {
+        value = var.organization
+}
---
+++ terraform.auto.tfvars
@@ -0,0 +1,71 @@
+#Have all these as hidden vars
+#This is the Intersight API key
+#api_key = ""
+
+#This is the IS secret key
+#secretkey = ""
+
+#This is the k8s node ssh key
+#mgmtcfgsshkeys = ""
+
+#This is the vsphere target admin password
+#password = ""
+
+#This is the terraform cloud user key
+#cloud_api_key_id = ""
+
+#Common workspaces
+ikswsname = "rtp-iks-cluster-01"
+globalwsname = "rtp-iks-cluster-01-global"
+
+
+#Configurable and to be uploaded based on sandbox env
+clustername = "rtp-iks-cluster-01"
+ippool_list = "rtp-iks-cluster-01_ippool"
+
+#All these are params used in k8s policy settings
+infra_config_policy = "rtp-iks-cluster-01_infra_config_policy"
+ip_pool_policy = "rtp-iks-cluster-01_ip_pool_policy"
+masterip_pool_policy = "rtp-iks-cluster-01_ip_pool_policy"
+workerip_pool_policy = "rtp-iks-cluster-01_ip_pool_policy"
+network_pod = "rtp-iks-cluster-01_pod"
+network_service = "rtp-iks-cluster-01_network"
+k8s_version = "1.19.5"
+k8s_version_name = "rtp-iks-cluster-01_1.19.5"
+masterinstance = "rtp-iks-cluster-01-master-instance"
+mgmtcfgetcd = false
+mgmtcfglbcnt = 3
+mgmtcfgsshuser = "iksadmin"
+mastergrpname = "rtp-iks-cluster-01-master-pool"
+masterdesiredsize = 1
+workergrpname = "rtp-iks-cluster-01-worker-pool"
+workerdesiredsize = 0
+masterinfraname = "rtp-iks-cluster-01-masterinfraname"
+infrapolname = "rtp-iks-cluster-01_infra_config_policy"
+instancetypename = "rtp-iks-cluster-01-master-instance"
+#network params
+timezone = "America/New_York"
+domain_name = "dcv.svpod"
+
+
+
+#All these are params used in Infra setup, capture specifics to each of the env's this is being deployed on
+#device_name = "10.200.0.210"
+#portgroup = ["Management"]
+#datastore = "hx-demo-ds1"
+#vspherecluster = "hx-demo"
+#resource_pool = "Test_Resource_Pool"
+#organization = "default"
+
+#ip_pool_policy params
+#starting_address = "10.200.0.20"
+#pool_size = "30"
+#netmask = "255.255.255.0"
+#gateway = "10.200.0.254"
+#primary_dns = "10.200.0.100"
+
+
+#instance type
+#cpu = "4"
+#disk_size = "40"
+#memory = "16384"
