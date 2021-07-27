f30f4380ba3e98f9b2867840cdf01ae986d66862
andrewwa-cisco <87471918+andrewwa-cisco@users.noreply.github.com>
2021-07-27 11:56:47 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -216,6 +216,14 @@ output "primary_dns" {
 }
 
 
+variable "primary_ntp" {
+  type = string
+}
+output "primary_ntp" {
+        value = var.primary_ntp
+}
+
+
 
 variable "timezone" {
   type = string
